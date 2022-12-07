934aeb967db15f87bee8b7b64ed0b127b7073d13
Kevin Ta <kvnta@yahoo.com>
2022-12-07 00:52:54 -0700

    Can return information for one particular item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::ItemsController < ApplicationController
   def index
         render json: ItemSerializer.new(Item.all)
   end
+
+  def show 
+    render json: Item.find(params[:id]) 
+  end
+
+  
 end 
